b348853130556792c3e3fd59f53b3c3ce2eac35f
sgs006 <34920116+sgs006@users.noreply.github.com>
2018-05-10 14:46:24 -0500

    Create designs.js

---
+++ designs.js
@@ -0,0 +1,39 @@
+// Select color input
+const color = $('#colorPicker');
+const table = $('#pixelCanvas');
+// Select size input
+const height = $('#inputHeight');
+const width = $('#inputWeight');
+  
+
+ 
+// When size is submitted by the user, call makeGrid()
+
+$('#submit').on('click', function makeGrid(event) {
+  event.preventDefault(); //prevent page refresh
+  table.children().remove();//clear grid and restart
+  let rows = height.val();
+  let cols = width.val();
+  for (let m = 0; m < rows; m++){
+    table.append('<tr></tr>');
+  }
+   for(let n = 0; n < cols; n++){
+    $('table tr').append('<td class="box"></td>');
+  }
+});
+
+table.on('click', '.box', function(){
+  let background = color.val();
+  if( $(this).css("background-color") === 'rgba(0, 0, 0, 0)' ||       $(this).css("background-color") === 'transparent'){
+     $(this).css('background-color', background);
+  }else{
+     $(this).css('background-color', 'transparent');
+  }
+});
+
+$("table").on("mousemove mouseenter mouseleave mouseover", "td", function(e) {
+    if (e.which === 1) {
+      let background = color.val();
+      $(this).css("background-color", background);
+    }
+  });
